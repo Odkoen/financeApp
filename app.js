eb899f99807bf2edb085d1ed41920868120b96ce
odko <freeloff07@gmail.com>
2021-01-23 07:39:26 -0800

    finance module ruu additem

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var uiController = (function () {
   return {
     getInput: function () {
       return {
-        type: document.querySelector(DOMstrings.inputType).value,
+        type: document.querySelector(DOMstrings.inputType).value, // exp, inc
         description: document.querySelector(DOMstrings.inputDescription).value,
         value: document.querySelector(DOMstrings.inputValue).value,
       };
@@ -19,35 +19,66 @@ var uiController = (function () {
     },
   };
 })();
+
+
+
+
 // Sanhuutei ajillah controller
 var financeController = (function () {
   var Income = function(id, description, value){
-    this.is = id;
+    this.id = id;
     this.description = description;
     this.value = value;
   }
   var Expense = function(id, description, value){
-    this.is = id;
+    this.id = id;
     this.description = description;
     this.value = value;
   }
   var data = {
-    allItems: {
+    items: {
       inc: [],
       exp: []
-    }
+    },
     totals: {
       inc: 0,
-      exp: 0,
+      exp: 0
+    }
+  }
+  return {
+    addItem: function(type, desc, val){
+      var item, id;
+      if(data.items[type].length === 0) id = 1;
+      else {
+        id = data.items[type][data.items[type].length - 1].id +1; 
+      }
+
+      //identification  
+      if(type === 'inc'){
+       item = new Income(id, desc, val);
+      }else{
+       item = new Expense(id, desc, val);
+      }
+      data.items[type].push(item);
+    },
+    seeData: function(){
+      return data;
     }
   }
 })();
+
+
+
+
+
+
 // Programm holbogch controller
 var appController = (function (uiController, financeController) {
-  var addItem = function () {
-    console.log(uiController.getInput());
+  var ctrlAddItem = function () {
     //oruulah ugugdul delgetsnees avna
+    var input = uiController.getInput();
     //avsn ugugdluu sanhuugiin controllerd damjuulna
+    financeController.addItem(input.type, input.description, input.value);
     //olj avsn ugugdluu orlogo zarlagad bairluulna
     //tusviig tootsoolno
     //etssiin uldegdel tootsoog delgetsend gargana
@@ -56,16 +87,17 @@ var appController = (function (uiController, financeController) {
   var setupEventListener = function () {
     DOM = uiController.getDomstrings();
     document.querySelector(DOM.addBtn).addEventListener("click", function () {
-      addItem();
+      ctrlAddItem();
     });
     document.addEventListener("keypress", function (event) {
       if (event.keyCode === 13 || event.which === 13) {
-        addItem();
+        ctrlAddItem();
       }
     });
   };
   return {
     init: function () {
+      console.log("app started")
       setupEventListener();
     },
   };
